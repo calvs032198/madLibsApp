9aa4bed8de5f0325030f7433cf16b1674fb7e32a
Calvin Mañalac <calvinmanalac21@gmail.com>
2022-06-08 20:39:26 +0800

    questionArray and questionCouter added

--- app.js
+++ app.js
@@ -3,5 +3,14 @@ let userInputs = [
   "found herself working on a",
   'word got out that the team had "debugged the"  ',
 ];
+let questionArray = [];
+let questionCounter = 0;
 
-console.log(userInputs);
+for (let i = 0; i < 5; i++) {
+  questionCounter += 1;
+  questionArray.push(prompt("Enter A verb"));
+  questionArray.push(prompt("Enter A noun"));
+}
+
+console.log(questionArray);
+console.log(questionCounter);
